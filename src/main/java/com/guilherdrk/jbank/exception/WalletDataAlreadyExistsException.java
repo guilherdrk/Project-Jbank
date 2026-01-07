c372c573235d09f1b6b4c7e9764958c5d2d9017c
guilherdrk <guibem20@gmail.com>
2026-01-07 15:14:36 -0300

    Handling exceptions globally with @RestControllerAdvice

--- src/main/java/com/guilherdrk/jbank/exception/WalletDataAlreadyExistsException.java
+++ src/main/java/com/guilherdrk/jbank/exception/WalletDataAlreadyExistsException.java
@@ -1,8 +1,26 @@
 package com.guilherdrk.jbank.exception;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 public class WalletDataAlreadyExistsException extends JBankException{
 
-    public WalletDataAlreadyExistsException(String message) {
-        super(message);
+    private final String detail;
+
+    public WalletDataAlreadyExistsException(String detail) {
+        super(detail);
+        this.detail = detail;
+    }
+
+    @Override
+    public ProblemDetail toProblemDetail() {
+        var pd = ProblemDetail.forStatus(422);
+
+        pd.setTitle("Wallet data already exists");
+        pd.setDetail(detail);
+
+        return pd;
     }
 }
